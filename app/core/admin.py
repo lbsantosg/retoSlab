e4d6eebdee5d9e8705f875ca25635f67d59c6345
Laura Santos <lbsantosg@unal.edu.co>
2021-07-12 13:30:10 -0500

    fix little errors

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,12 +11,12 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name', )}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
                 'fields': (
-                    'is_active', 
+                    'is_active',
                     'is_staff',
                     'is_superuser'
                 )
