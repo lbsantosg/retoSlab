387df8c37ff38b6a4224e198afe09508e465a082
Laura Santos <lbsantosg@unal.edu.co>
2021-07-12 17:35:37 -0500

    add wait for db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,21 +4,32 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
 
+
 class CommandsTests(TestCase):
-    pass
+    """
+    Management command: helper command that allows us to wait for de db to be
+    available before continuing and running other commands.
+    """
 
     def test_wait_for_db_ready(self):
-        """Test waiting for db when db is available"""
+        """
+        Tests What happens when we call our command and the db is already available.
+        Tests waiting for db when db is available
+        """
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
 
-
-    @patch('time.sleep', return_value=True)
+    @patch('time.sleep', return_value=None)
     def test_wait_for_db(self, ts):
-        """Test wating for db"""
-        with patch('django.db.utils.ConnectionHander.__getitem__') as gi:
+        """
+        Cheks that the wait_for_db command will try the db 5 and on the 6th
+        it will be succesfull and we will contintue
+        Test wating for db
+        """
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
