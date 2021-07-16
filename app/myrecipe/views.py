a6bb433987bb27b16bdfd79e0e95f3053bfc92b1
Laura Santos <lbsantosg@unal.edu.co>
2021-07-16 13:26:05 -0500

    Added create tag feature

--- app/myrecipe/views.py
+++ app/myrecipe/views.py
@@ -6,8 +6,19 @@ from core.models import Tag
 from myrecipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage tags in the database """
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
+    """
+        Manage tags in the database
+        You can add mixins to customize the fucntionality that is
+        available for our viewset so you don't have to do the
+        standartd model viweset and accept everything because there
+        might be some cases where you don't want to allow users to
+        do everything and you don't need that feature so ther's no
+        point implementing it. So, you can customize it by adding the
+        mixins for what you want to do. 
+    """
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
     queryset = Tag.objects.all()
@@ -22,3 +33,13 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
             displayed in the API
         """
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """
+            Create new tag.
+            When we do a create object in our viewset this function will
+            be invoked and the validated serializer will be in as a serializer
+            argument and then we can perform any modifications here that
+            we'd like to in our create process.
+        """
+        serializer.save(user=self.request.user)
